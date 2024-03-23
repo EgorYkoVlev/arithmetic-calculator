e9a8af58bfa842890f993f47792259b53026a52c
Egor Yakovlev <egoryak08@gmail.com>
2024-03-23 18:39:14 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        ArithmeticCalculator calculator = new ArithmeticCalculator(9,87);
+        System.out.println(calculator.calculate(Operation.ADD) + "\n" +
+                calculator.calculate(Operation.SUBTRACT) + "\n" +
+                calculator.calculate(Operation.MULTIPLY));
+    }
+}
