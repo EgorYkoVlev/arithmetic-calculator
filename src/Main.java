775b77a333626ea25b516c613312cc1e6f4029a0
EgorYkoVlev <164177326+EgorYkoVlev@users.noreply.github.com>
2024-03-24 18:00:13 +0300

    + one string and one number's changed

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        ArithmeticCalculator calculator = new ArithmeticCalculator(9,87);
+        ArithmeticCalculator calculator = new ArithmeticCalculator(9,10);
         System.out.println(calculator.calculate(Operation.ADD) + "\n" +
                 calculator.calculate(Operation.SUBTRACT) + "\n" +
                 calculator.calculate(Operation.MULTIPLY));
+        System.out.println("That's all for the now");
     }
 }
